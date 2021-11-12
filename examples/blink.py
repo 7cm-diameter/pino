0a7e72c037cffc547b1788d04bbfa50659513431
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-11-12 19:55:11 +0900

    Add an example of using `Config`

--- examples/blink.py
+++ examples/blink.py
@@ -1,20 +1,27 @@
 if __name__ == '__main__':
     from time import sleep
 
+    from pino.config import Config
     from pino.ino import HIGH, LOW, OUTPUT, Arduino, Comport
 
-    com = Comport() \
-        .set_port("/dev/ttyACM0") \
-        .set_baudrate(115200) \
-        .set_timeout(1.) \
-        .set_warmup(2.) \
+    config = Config("./examples/sample.yaml")
+    com = Comport.derive(config.comport) \
         .deploy() \
         .connect()
 
+    # com = Comport() \
+    #     .set_port("/dev/ttyACM0") \
+    #     .set_baudrate(115200) \
+    #     .set_timeout(1.) \
+    #     .set_warmup(2.) \
+    #     .deploy() \
+    #     .connect()
+
     ino = Arduino(com)
+    ino.apply_pinmode_settings(config.pinmode)
 
     LED_BUILTIN = 13
-    ino.set_pinmode(LED_BUILTIN, OUTPUT)
+    # ino.set_pinmode(LED_BUILTIN, OUTPUT)
 
     for _ in range(10):
         ino.digital_write(LED_BUILTIN, HIGH)
